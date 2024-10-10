386a995a565f4e4dcb2397728b9300f203d78a93
Ashra <minhajsorder8205@gmail.com>
2024-10-10 15:15:45 +0600

    msdsaf

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -5,7 +5,8 @@ import axios, { Axios } from 'axios';
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-export const backendUrlLink = 'http://localhost:5003';
+// export const backendUrlLink = 'http://localhost:5003';
+export const backendUrlLink = 'http://85.31.233.92:5003';
 export const api = axios.create({
   withCredentials: true,
   baseURL: backendUrlLink,
---
+++ production_frontend/assets/use-quasar-a2ac154a.js
@@ -0,0 +1,1 @@
+import{i as a,d3 as r}from"./index-50a4d5a2.js";function u(){return a(r)}export{u};
